c917199ebd12a46dd4f52a7599d69ee31a96d01f
Konru <konrumi@gmail.com>
2017-05-08 23:17:03 +0800

    complete data

--- scripts/getherData.js
+++ scripts/getherData.js
@@ -1,13 +1,15 @@
 (function() {
     var $config       = {
-            'group': '动物 FAUNA',
-            'phylum': '脊索动物门 PHYLUM CHORDATA',
-            'class': '哺乳纲 MAMMALIA',
-            'order': '食肉目 CARNIVORA'
+            'group': '植物 FLORA',
+            'phylum': '',
+            'class': '',
+            // 'order': document.querySelectorAll('.theTitle .btt')[0].innerText.replace(/(^.*[^a-zA-Z])([a-zA-Z].*$)/g, '$1 $2')
+            'order': ''
         },
 
         $result       = {},
 
+        currentOrder  = $config.order,
         currentFamily = '';
 
     document.querySelectorAll('table.MsoNormalTable')[0].querySelectorAll('tr').forEach(
@@ -16,23 +18,38 @@
             var tds = trs.querySelectorAll('td');
 
             if (tds.length === 1) {
-                // family line
-                var familyText = tds[0].innerText;
-                familyText = familyText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '');
+                if (tds[0].querySelectorAll('a').length > 0 && $config.order === '') {
+                    // order line
+                    currentOrder = tds[0].innerText;
+                    currentOrder = currentOrder.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '');
+                } else {
+                    // family line
+                    var familyText = tds[0].innerText;
+                    familyText = familyText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '');
 
-                if (familyText.match(/[^a-zA-Z][a-zA-Z]/g) !== null) {
-                    familyText = familyText.replace(/([^a-zA-Z])([a-zA-Z])/g, '$1 $2');
+                    if (familyText.match(/[^a-zA-Z][a-zA-Z]/g) !== null) {
+                        familyText = familyText.replace(/([^a-zA-Z])([a-zA-Z])/g, '$1 $2');
+                    }
+
+                    currentFamily = familyText;
                 }
-
-                currentFamily = familyText;
             } else {
                 if (
-                    tds[0].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅰ' &&
-                    tds[1].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅱ' &&
-                    tds[2].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅲ'
+                    tds[0].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅰ' && tds[0].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录I' &&
+                    tds[1].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅱ' && tds[1].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录II' &&
+                    tds[2].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录Ⅲ' && tds[2].innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '') !== '附录III'
                 ) {
                     // content
                     tds.forEach(function(td, idx) {
+                        /* // for FLORA
+                        if (td.innerText === '') {
+                            return;
+                        }
+                        if (td.querySelectorAll('p').length === 0 && td.querySelectorAll('a').length > 0) {
+                            currentFamily = td.innerText;
+                        }
+                        // for FLORA */
+
                         var type = idx + 1;
                         td.querySelectorAll('p').forEach(function(p) {
                             var elementText = p.innerText.replace(/^[\s\t\r\n]|[\s\t\r\n]$/g, '');
@@ -51,7 +68,7 @@
                                     'group': $config.group,
                                     'phylum': $config.phylum,
                                     'class': $config.class,
-                                    'order': $config.order,
+                                    'order': currentOrder,
                                     'family': currentFamily,
                                     'binomial': binomial,
                                     'cnName': cnName,
